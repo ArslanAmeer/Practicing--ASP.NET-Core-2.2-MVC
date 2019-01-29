ad9bd817a0fea4fcb90fdd6e650e11df2aad5abb
ArslanAmeer <arslan.ameer@hotmail.com>
2019-01-29 16:48:19 +0500

    🎉👌 Add: Display ProductData from Db to View

--- GraniteHouse/Areas/Admin/Controllers/ProductTypeController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraniteHouse.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraniteHouse.Areas.Admin.Controllers
@@ -9,9 +10,19 @@ namespace GraniteHouse.Areas.Admin.Controllers
     [Area("Admin")]
     public class ProductTypeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            using (_db)
+            {
+                return View(_db.ProductTypes.ToList());
+            }
         }
     }
 }
