61ef6f56fca64012fb97a9676f874055784acb68
ArslanAmeer <arslan.ameer@hotmail.com>
2019-02-07 12:05:05 +0500

    ♻👍 Update: Startup Page Change

--- GraniteHouse/Startup.cs
+++ GraniteHouse/Startup.cs
@@ -77,7 +77,7 @@ namespace GraniteHouse
             {
                 routes.MapRoute(
                     name: "areas",
-                    template: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
+                    template: "{area=Admin}/{controller=ProductType}/{action=Index}/{id?}"
                 );
             });
         }
